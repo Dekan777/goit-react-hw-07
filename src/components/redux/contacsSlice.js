ed32860526e0d6e9f0cf51fc70accf42c72785c3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-12 23:28:58 +0100

    slice

--- src/components/redux/contacsSlice.js
+++ src/components/redux/contacsSlice.js
@@ -8,6 +8,20 @@ export const contacsSlice = createSlice({
         error: null
     },
     reducers: {
+
+        fetchingInProgress(state) {
+            state.isLoading = true;
+        },
+        fetchingSuccess(state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items = action.payload;
+        },
+        fetchingError(state, action) {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+
         addContact: {
             reducer(state, action) {
                 state.contacts.push(action.payload);
