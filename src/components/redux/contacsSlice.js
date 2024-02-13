a2e2b686f0b42395c89dc1b960912e47059ecb6f
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-13 01:06:52 +0100

    payloadCreator

--- src/components/redux/contacsSlice.js
+++ src/components/redux/contacsSlice.js
@@ -1,42 +1,61 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts } from './operations'
 
-export const contacsSlice = createSlice({
+const contacsSlice = createSlice({
     name: 'contacts',
     initialState: {
         contacts: [],
         isLoading: false,
         error: null
     },
-    reducers: {
-
-        fetchingInProgress(state) {
-            state.isLoading = true;
-        },
-        fetchingSuccess(state, action) {
-            state.isLoading = false;
-            state.error = null;
-            state.items = action.payload;
-        },
-        fetchingError(state, action) {
-            state.isLoading = false;
-            state.error = action.payload;
-        },
-
-        addContact: {
-            reducer(state, action) {
-                state.contacts.push(action.payload);
-            }
-        },
-        removeContact: {
-            reducer(state, action) {
-                state.contacts = state.contacts.filter(
-                    contact => contact.id !== action.payload
-                );
-            }
-        }
-    }
+    // Додаємо обробку зовнішніх екшенів
+    extraReducers: builder => {
+        builder
+            .addCase(fetchContacts.pending, (state, action) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchContacts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+            })
+            .addCase(fetchContacts.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
+
+export const tasksReducer = contacsSlice.reducer;
+// reducers: {
+
+//     fetchingInProgress(state) {
+//         state.isLoading = true;
+//     },
+//     fetchingSuccess(state, action) {
+//         state.isLoading = false;
+//         state.error = null;
+//         state.items = action.payload;
+//     },
+//     fetchingError(state, action) {
+//         state.isLoading = false;
+//         state.error = action.payload;
+//     },
+
+//     addContact: {
+//         reducer(state, action) {
+//             state.contacts.push(action.payload);
+//         }
+//     },
+//     removeContact: {
+//         reducer(state, action) {
+//             state.contacts = state.contacts.filter(
+//                 contact => contact.id !== action.payload
+//             );
+//         }
+//     }
+// }
 
 export const { fetchingInProgress, fetchingSuccess, fetchingError, addContact, removeContact } = contacsSlice.actions;
 
--- src/components/redux/operations.js
+++ src/components/redux/operations.js
@@ -1,38 +1,47 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import {
-    fetchingInProgress,
-    fetchingSuccess,
-    fetchingError,
-} from "./contacsSlice";
 axios.defaults.baseURL = 'https://65c9383ba4fbc162e112b881.mockapi.io';
 
+// import {
+//     fetchingInProgress,
+//     fetchingSuccess,
+//     fetchingError,
+// } from "./contacsSlice";
 
-export const fetchContacts = () => async dispatch => {
-    try {
-        // Індикатор завантаження
-        dispatch(fetchingInProgress());
-        // HTTP-запит
-        const response = await axios.get("/contacts");
-        // Обробка даних
-        dispatch(fetchingSuccess(response.data));
-    } catch (e) {
-        // Обробка помилки
-        dispatch(fetchingError(e.message));
-    }
-};
+
+// export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
+//     const response = await axios.get("/contacts");
+//     return response.data;
+// });
 
 // export const fetchContacts = createAsyncThunk(
-//     'contacts/fetchAll',
+//     "tasks/fetchAll",
+//     // Використовуємо символ підкреслення як ім'я першого параметра,
+//     // тому що в цій операції він нам не потрібен
 //     async (_, thunkAPI) => {
 //         try {
-//             const response = await axios.get('/contacts');
+//             const response = await axios.get("/contacts");
+//             // При успішному запиті повертаємо проміс із даними
 //             return response.data;
-//         } catch (error) {
-//             return thunkAPI.rejectWithValue(error.message);
+//         } catch (e) {
+//             // При помилці запиту повертаємо проміс
+//             // який буде відхилений з текстом помилки
+//             return thunkAPI.rejectWithValue(e.message);
 //         }
 //     }
 // );
+
+export const fetchContacts = createAsyncThunk(
+    'contacts/fetchAll',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get('/contacts');
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
 
 // export const addContact = createAsyncThunk(
 //     'contacts/addContact',
